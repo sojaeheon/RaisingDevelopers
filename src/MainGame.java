759bcdfaf091580952b78dcf65b518f8f874ae7e
sojaeheon <sojaeheon123@daum.net>
2023-11-09 16:18:12 +0900

    타이머

--- src/MainGame.java
+++ src/MainGame.java
@@ -15,8 +15,7 @@ import dto.QuizQuestion;
 public class MainGame {
 	
 	private int chap_num;
-	private float score=0;
-	private int count=0;
+	
 	
 	//sql관련
 	private Connection con=null;
@@ -33,6 +32,7 @@ public class MainGame {
 	private JTextField input_area;
 	private JLabel lbl_quesnum;
 	private JLabel lbl_chapnum;
+	private JLabel timerLabel;
 	
 	private Font font = new Font("Monospaced",Font.BOLD,20);
 
@@ -58,7 +58,7 @@ public class MainGame {
 	public MainGame(int chap_num) {
 		this.chap_num=chap_num;
 		initialize();
-		SqlConnection();
+		
 	}
 
 	/**
@@ -66,9 +66,10 @@ public class MainGame {
 	 */
 	private void initialize() {
 		
+		
 		//프레임
 		frame = new JFrame();
-		frame.setBounds(100, 100, 900, 800);
+		frame.setBounds(100, 100, 980, 675);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
@@ -87,14 +88,21 @@ public class MainGame {
 		frame.getContentPane().add(input_area);
 		input_area.setColumns(10);
 		
-		//제출버튼
-		JButton btn_submit = new JButton("제출");
-		btn_submit.setBounds(369, 561, 97, 23);
-		frame.getContentPane().add(btn_submit);
+		//타이머 스레드
+		timerLabel = new JLabel();
+		timerLabel.setOpaque(true);
+		timerLabel.setBounds(836, 136, 57, 30);
+		timerLabel.setForeground(Color.black);
+		Timer timer = new Timer(timerLabel,20);
+		frame.getContentPane().add(timerLabel);
 		
-		JLabel lbl_timer = new JLabel("New label");
-		lbl_timer.setBounds(707, 99, 57, 15);
-		frame.getContentPane().add(lbl_timer);
+		timer.start();
+		
+		
+		
+		
+
+		
 		
 		lbl_chapnum = new JLabel("Chapter : "+ Integer.toString(chap_num));
 		lbl_chapnum.setBounds(392, 63, 84, 15);
@@ -104,6 +112,7 @@ public class MainGame {
 		lbl_quesnum.setBounds(84, 99, 57, 15);
 		frame.getContentPane().add(lbl_quesnum);
 		
+		//이전 문제 이동
 		JButton btn_left = new JButton("<");
 		btn_left.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -114,6 +123,7 @@ public class MainGame {
 		btn_left.setBounds(305, 520, 97, 23);
 		frame.getContentPane().add(btn_left);
 		
+		//다음 문제 이동
 		JButton btn_right = new JButton(">");
 		btn_right.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -124,17 +134,209 @@ public class MainGame {
 		btn_right.setBounds(432, 520, 97, 23);
 		frame.getContentPane().add(btn_right);
 		
+		//1번 문제로 이동
+		JButton btn_1 = new JButton("1번 문제");
+		btn_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				currentUserAnswer = input_area.getText();
+				user_ansArray[currentQuestionIndex] = currentUserAnswer;
+				
+				currentQuestionIndex = 0;
+				QuizQuestion currentQuestion = questions.get(currentQuestionIndex);
+		    	System.out.println(currentQuestion.getQuestion());
+		        issues_area.setText(currentQuestion.getQuestion());
+		        input_area.setText(user_ansArray[currentQuestionIndex]);
+				
+			}
+		});
+		btn_1.setBounds(803, 173, 97, 23);
+		frame.getContentPane().add(btn_1);
+		
+		//2번 문제로 이동
+		JButton btn_2 = new JButton("2번 문제");
+		btn_2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				currentUserAnswer = input_area.getText();
+				user_ansArray[currentQuestionIndex] = currentUserAnswer;
+				
+				currentQuestionIndex = 1;
+				QuizQuestion currentQuestion = questions.get(currentQuestionIndex);
+		    	System.out.println(currentQuestion.getQuestion());
+		        issues_area.setText(currentQuestion.getQuestion());
+		        input_area.setText(user_ansArray[currentQuestionIndex]);
+			}
+		});
+		btn_2.setBounds(803, 206, 97, 23);
+		frame.getContentPane().add(btn_2);
+		
+		//3번 문제로 이동
+		JButton btn_3 = new JButton("3번 문제");
+		btn_3.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				currentUserAnswer = input_area.getText();
+				user_ansArray[currentQuestionIndex] = currentUserAnswer;
+				
+				currentQuestionIndex = 2;
+				QuizQuestion currentQuestion = questions.get(currentQuestionIndex);
+		    	System.out.println(currentQuestion.getQuestion());
+		        issues_area.setText(currentQuestion.getQuestion());
+		        input_area.setText(user_ansArray[currentQuestionIndex]);
+			}
+		});
+		btn_3.setBounds(803, 239, 97, 23);
+		frame.getContentPane().add(btn_3);
+		
+		//4번 문제로 이동
+		JButton btn_4 = new JButton("4번 문제");
+		btn_4.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				currentUserAnswer = input_area.getText();
+				user_ansArray[currentQuestionIndex] = currentUserAnswer;
+				
+				currentQuestionIndex = 3;
+				QuizQuestion currentQuestion = questions.get(currentQuestionIndex);
+		    	System.out.println(currentQuestion.getQuestion());
+		        issues_area.setText(currentQuestion.getQuestion());
+		        input_area.setText(user_ansArray[currentQuestionIndex]);
+			}
+		});
+		btn_4.setBounds(803, 272, 97, 23);
+		frame.getContentPane().add(btn_4);
+		
+		//5번 문제로 이동
+		JButton btn_5 = new JButton("5번 문제");
+		btn_5.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				currentUserAnswer = input_area.getText();
+				user_ansArray[currentQuestionIndex] = currentUserAnswer;
+				
+				currentQuestionIndex = 4;
+				QuizQuestion currentQuestion = questions.get(currentQuestionIndex);
+		    	System.out.println(currentQuestion.getQuestion());
+		        issues_area.setText(currentQuestion.getQuestion());
+		        input_area.setText(user_ansArray[currentQuestionIndex]);
+			}
+		});
+		btn_5.setBounds(803, 305, 97, 23);
+		frame.getContentPane().add(btn_5);
+		
+		//6번 문제로 이동
+		JButton btn_6 = new JButton("6번 문제");
+		btn_6.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				currentUserAnswer = input_area.getText();
+				user_ansArray[currentQuestionIndex] = currentUserAnswer;
+				
+				currentQuestionIndex = 5;
+				QuizQuestion currentQuestion = questions.get(currentQuestionIndex);
+		    	System.out.println(currentQuestion.getQuestion());
+		        issues_area.setText(currentQuestion.getQuestion());
+		        input_area.setText(user_ansArray[currentQuestionIndex]);
+			}
+		});
+		btn_6.setBounds(803, 338, 97, 23);
+		frame.getContentPane().add(btn_6);
+		
+		//7번 문제로 이동
+		JButton btn_7 = new JButton("7번 문제");
+		btn_7.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				currentUserAnswer = input_area.getText();
+				user_ansArray[currentQuestionIndex] = currentUserAnswer;
+				
+				currentQuestionIndex = 6;
+				QuizQuestion currentQuestion = questions.get(currentQuestionIndex);
+		    	System.out.println(currentQuestion.getQuestion());
+		        issues_area.setText(currentQuestion.getQuestion());
+		        input_area.setText(user_ansArray[currentQuestionIndex]);
+			}
+		});
+		btn_7.setBounds(803, 371, 97, 23);
+		frame.getContentPane().add(btn_7);
+		
+		//8번 문제로 이동
+		JButton btn_8 = new JButton("8번 문제");
+		btn_8.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				currentUserAnswer = input_area.getText();
+				user_ansArray[currentQuestionIndex] = currentUserAnswer;
+				
+				currentQuestionIndex = 7;
+				QuizQuestion currentQuestion = questions.get(currentQuestionIndex);
+		    	System.out.println(currentQuestion.getQuestion());
+		        issues_area.setText(currentQuestion.getQuestion());
+		        input_area.setText(user_ansArray[currentQuestionIndex]);
+			}
+		});
+		btn_8.setBounds(803, 404, 97, 23);
+		frame.getContentPane().add(btn_8);
+		
+		//9번 문제로 이동
+		JButton btn_9 = new JButton("9번 문제");
+		btn_9.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				currentUserAnswer = input_area.getText();
+				user_ansArray[currentQuestionIndex] = currentUserAnswer;
+				
+				currentQuestionIndex = 8;
+				QuizQuestion currentQuestion = questions.get(currentQuestionIndex);
+		    	System.out.println(currentQuestion.getQuestion());
+		        issues_area.setText(currentQuestion.getQuestion());
+		        input_area.setText(user_ansArray[currentQuestionIndex]);
+			}
+		});
+		btn_9.setBounds(803, 437, 97, 23);
+		frame.getContentPane().add(btn_9);
+		
+		//10번 문제로 이동
+		JButton btn_10 = new JButton("10번 문제");
+		btn_10.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				currentUserAnswer = input_area.getText();
+				user_ansArray[currentQuestionIndex] = currentUserAnswer;
+				
+				currentQuestionIndex = 9;
+				QuizQuestion currentQuestion = questions.get(currentQuestionIndex);
+		    	System.out.println(currentQuestion.getQuestion());
+		        issues_area.setText(currentQuestion.getQuestion());
+		        input_area.setText(user_ansArray[currentQuestionIndex]);
+			}
+		});
+		btn_10.setBounds(803, 470, 97, 23);
+		frame.getContentPane().add(btn_10);
+		
+		//제출버튼
+		JButton btn_submit = new JButton("제출");
+		btn_submit.setBounds(855, 520, 97, 23);
+		frame.getContentPane().add(btn_submit);
 		btn_submit.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+        		timer.interrupt();
+        		
                 checkAnswer();
+                
             }
 
         });
 		
-		
-
+	
 		frame.setVisible(true);
+//		frame.getContentPane().addKeyListener(new EnterKeyListener());
+//		frame.getContentPane().setFocusable(true);
+//		frame.getContentPane().requestFocus();
+		
+				
+		SqlConnection();
+		
+		while(timer.isAlive()) {
+			if(timer.getState() == timer.getState().TERMINATED) {
+				checkAnswer();
+			}
+			
+		}
+		//엔터키 이벤트 처리
+		
 		
 		
 	}
@@ -160,8 +362,9 @@ public class MainGame {
             System.out.println("연결 완료!");
             
             // 데이터베이스에서 문제 불러오는 쿼리를 실행
-            String sql = "SELECT problem, answer FROM question where chap_num = "+chap_num;
+            String sql = "SELECT problem, answer FROM question where chap_num = " + chap_num;
             
+            System.out.println(chap_num);
             //연결한 db(con)에 sql을 넣고, 나온 값은 statement에 저장
             PreparedStatement statement = con.prepareStatement(sql);
             resultSet = statement.executeQuery();
@@ -191,6 +394,7 @@ public class MainGame {
         
 	}
 	
+	//문제 출력
 	private void displayQuestion() {
     	QuizQuestion currentQuestion = questions.get(currentQuestionIndex);
         issues_area.setText(currentQuestion.getQuestion());
@@ -198,18 +402,22 @@ public class MainGame {
         
     }
 	
+	//다음 문제로 이동하는 함수
 	private void goToNextQuestion() {
-		if (currentQuestionIndex < questions.size() - 1) {
+		if (currentQuestionIndex < questions.size()-1) {
 			currentUserAnswer = input_area.getText();
     		user_ansArray[currentQuestionIndex] = currentUserAnswer;
             currentQuestionIndex++;
             lbl_quesnum.setText(Integer.toString(currentQuestionIndex+1));
             displayQuestion();
+            input_area.requestFocus();
             
         } else {
             JOptionPane.showMessageDialog(frame, "모든 문제를 푸셨습니다!");
         }
 	}
+	
+	//이전 문제로 이동하는 함수
 	private void goToPreviousQuestion() {
     	if (currentQuestionIndex > 0) {
     		currentUserAnswer = input_area.getText();
@@ -217,11 +425,20 @@ public class MainGame {
             currentQuestionIndex--;
 			lbl_quesnum.setText(Integer.toString(currentQuestionIndex+1));
             displayQuestion();
+            input_area.requestFocus();
         } else {
             JOptionPane.showMessageDialog(frame, "첫 번째 문제입니다.");
         }
     }
+	
+	//답 제출 함수
 	private void checkAnswer() {
+		currentUserAnswer = input_area.getText();
+		user_ansArray[currentQuestionIndex] = currentUserAnswer;
+		
+		float score=0;
+		int count=0;
+		
 		for(int i =0;i<questions.size();i++) {
 			try {
 				if(user_ansArray[i].equals(questions.get(i).getAnswer())) {
@@ -234,7 +451,18 @@ public class MainGame {
 			
 		}
 
-        JOptionPane.showMessageDialog(frame, "총 10문제 중 "+count+"정답 \n 획득 점수는 "+score+"입니다!!");
+
+        JOptionPane.showMessageDialog(frame, "총 10문제 중 "+count+"정답 \n 획득 점수는 "+count*10+"입니다!!");
         
 	}
+	
+	class EnterKeyListener extends KeyAdapter{
+		public void keyPressed(KeyEvent e) {
+			if (e.getKeyCode() == KeyEvent.VK_F5) {
+				goToNextQuestion();
+			}
+		}
+	}
 }
+
+
